98a61d4d0192c6cda3d24412e4a7857e4e19cd52
Meghana Dixit <meghamarao4240@gmail.com>
2023-03-29 19:46:42 -0700

    Adding a file in Log4jProject

---
+++ src/Log4jProj/TestLog4j.java
@@ -0,0 +1,13 @@
+package Log4jProj;
+
+import org.apache.log4j.Logger;
+
+public class TestLog4j {
+	
+	static Logger log = Logger.getLogger(TestLog4j.class);
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * @author neerajdixit
+ *
+ */
+module Log4jProject {
+	requires apache.log4j;
+}
